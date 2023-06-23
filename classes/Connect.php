3d97dd6cba76c24a383409e3b933de4b45caaf0c
srssaulo <srssaulo@gmail.com>
2023-06-23 11:55:48 -0300

    add Connect.php class that will make Govbr exchange

---
+++ classes/Connect.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace auth_neesgov;
+
+/**
+ * Implements 3 steps Gov.br authorization service.
+ * Class was implemented based in following sample:
+ * <https://manual-roteiro-integracao-login-unico.servicos.gov.br/pt/stable/arquivos/ExemploIntegracaoGovBr.java>
+ * Steps docs:
+ * <https://manual-roteiro-integracao-login-unico.servicos.gov.br/pt/stable/iniciarintegracao.html
+ *
+ * @author Saulo de Sá <srssaulo@gmail.com>
+ */
+class Connect
+{
+
+    private const URL_PROVIDER = "https://sso.staging.acesso.gov.br";
+    private const URL_SERVICOS = "https://api.staging.acesso.gov.br";
+    private const URL_CATALOGO_SELOS = "https://confiabilidades.staging.acesso.gov.br";
+    private const REDIRECT_URI = "<coloque-aqui-url-de-retorno>"; // redirectURI informada na chamada do serviço do
+    private const SCOPES = "openid+email+profile+govbr_empresa+govbr_confiabilidades"; // Escopos
+    private const CLIENT_ID = "<coloque-aqui-o-client-id>"; // clientId informado na chamada do serviço do authorize.
+    private const SECRET = "HJA8&A&*@!#(HSADNA<SU10*&!n"; // secret de conhecimento apenas do backend da aplicação.
+
+    private static string $CODE_CHALLENGE;
+    private const CODE_CHALLENGE_METHOD = "S256";
+
+
+    public function __construct()
+    {
+        self:self::setCodeChallange();
+    }
+
+    private static function setCodeChallange(){
+        self::$CODE_CHALLENGE = Utils::code_challange();
+    }
+
+    public function getCodeChallange(){
+        return self::$CODE_CHALLENGE;
+    }
+
+
+
+
+
+}
