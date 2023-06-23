25f6a259462ae286a78d5df7c32e3695aabae0c9
srssaulo <srssaulo@gmail.com>
2023-06-23 11:55:02 -0300

    first version of code_challange generator method

--- classes/Utils.php
+++ classes/Utils.php
@@ -5,7 +5,8 @@ namespace auth_neesgov;
 class Utils
 {
 
-    public static function get_nonce(){
+    public static function get_nonce()
+    {
         //echo \core\session\manager::get_login_token();
 //        echo \core\session\manager::validate_login_token()
         //Sequência de caracteres usado para associar uma sessão do serviço consumidor
@@ -15,25 +16,40 @@ class Utils
     }
 
 
-    public static function get_state(){
+    public static function get_state()
+    {
         //Parâmetro STATE deve obrigatoriamente ser usado e deve ser validado no cliente
         //(validado que foi previamente emitido pelo cliente)
-
         return \core\session\manager::get_login_token();
 
     }
 
-    public static function validate_state($state){
+    public static function validate_state($state)
+    {
         return \core\session\manager::validate_login_token($state);
     }
 
 
-    public static function code_challange(){
-        //https://devforum.okta.com/t/how-to-do-pkce-challenge-in-php/553
-//        https://medium.com/zenchef-tech-and-product/how-to-generate-a-pkce-challenge-with-php-fbee1fa29379
-
+    private static function base64url_encode($plainText){
+        $base64 = base64_encode($plainText);
+        $base64 = trim($base64, "=");
+        $base64url = strtr($base64, '+/', '-_');
+        return ($base64url);
     }
 
+    public static function code_challange(): string
+    {
+        //https://devforum.okta.com/t/how-to-do-pkce-challenge-in-php/553
+        //  https://medium.com/zenchef-tech-and-product/how-to-generate-a-pkce-challenge-with-php-fbee1fa29379
+        //second alternative:
+//        https://github.com/aweber/public-api-examples/blob/c28945cb419805bab30b4db9db41baf0df4338c8/php/get-pkce-access-token#L27
+
+        $random = bin2hex(openssl_random_pseudo_bytes(32));
+        $verifier = self::base64url_encode(pack('H*', $random));
+        $challenge = self::base64url_encode(pack('H*', hash('sha256', $verifier)));
+        return $challenge;
+
+    }
 
 
 }
