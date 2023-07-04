6cd9cfa5146c9729b2977ec45421f5b4fb855c1d
srssaulo <srssaulo@gmail.com>
2023-07-04 18:33:15 -0300

    neesflow ajusts, but not finished

--- classes/neesflow.php
+++ classes/neesflow.php
@@ -3,6 +3,7 @@
 namespace auth_neesgov;
 
 use auth_neesgov\httpclient;
+
 class neesflow
 {
 
@@ -10,20 +11,22 @@ class neesflow
     private \stdClass $params;
 
 
-    private function getTokenAndUserProfile(){
+    private function getTokenAndUserProfile()
+    {
         return (object)[
-            'accessToken'=>optional_param('accessToken', null, PARAM_RAW_TRIMMED),
-            'userProfile'=>optional_param('userProfile', 0, PARAM_INT),
+            'accessToken' => optional_param('accessToken', null, PARAM_RAW_TRIMMED),
+            'userProfile' => optional_param('userProfile', 0, PARAM_INT),
         ];
     }
 
-    public function handleGetUserNeesDataResults(){
-        if(empty($this->params)){
+    public function handleGetUserNeesDataResults()
+    {
+        if (empty($this->params)) {
             $this->params = $this->getTokenAndUserProfile();
         }
-        $this->httpClient =  new httpclient();
+        $this->httpClient = new httpclient();
         $userNees = $this->httpClient->getUserNeesData($this->params); //in moodle cpf is username //TODO PAREI AQUI
-        if($userNees->status_code){
+        if ($userNees->status_code) {
             throw new \moodle_exception("status {$userNees->status_code}: $userNees->detail", 'auth_neesgov');
         }
 
@@ -34,12 +37,16 @@ class neesflow
     }
 
     /**
-     * @param \stdClass $params atts: accesstoken, userProfile
+     * @param object $userInfo {'id'=>$oidc->requestUserInfo('sub'),
+                                'email'=>$oidc->requestUserInfo('email'),
+                                'name'=>$oidc->requestUserInfo('name'),
+                                'picture}
      * @return void
      */
-    private function handlelogin(){
+    private function handlelogin($userInfo)
+    {
         global $DB;
-        //look oidc authcode.php->handlelogin()
+
 
         // Do not continue if auth plugin is not enabled.
         if (!is_enabled_auth('neesgov')) {
@@ -47,15 +54,13 @@ class neesflow
         }
 
 
-        //TODO Reconhecimento do usuário no servidor externo
-        $neesUser = $this->handleGetUserNeesDataResults();
-        $mdlUser = $DB->get_record('user', ['username'=>trim($neesUser->cpf), 'deleted'=>0]);
+        $mdlUser = $DB->get_record('user', ['username' => trim($userInfo->id), 'deleted' => 0]);
 
-        if(!$mdlUser){
+        if (!$mdlUser) {
             throw new \moodle_exception('User doesn\'t created in moodle', 'auth_neesgov');
         }
 
-        if($mdlUser->auth!='neesgov'){//change user auth type to neesgov
+        if ($mdlUser->auth != 'neesgov') {//change user auth type to neesgov
             $mdlUser->auth = 'neesgov';
             $DB->update_record('user', $mdlUser);
         }
@@ -75,29 +80,30 @@ class neesflow
 
     }
 
-    public function handleRedirect(){
 
-        $redirect_url = trim(get_config('auth_neesgov', 'redirecturl'));
+    /**
+     * @param object $userInfo {'id'=>$oidc->requestUserInfo('sub'),
+                                'email'=>$oidc->requestUserInfo('email'),
+                                'name'=>$oidc->requestUserInfo('name'),
+                                'picture}
+     * @return void
+     * @throws \moodle_exception
+     */
+    public function handleRedirect($userInfo)
+    {
 
-        if(!empty($redirect_url)){
-            $dev_login_redirect = new \moodle_url($redirect_url);
-        }else{
-            $dev_login_redirect = new \moodle_url("https://develop-login-integracao-dot-scanner-prova.rj.r.appspot.com/login");
+        if(is_null($userInfo)){
+            //if null  didn't make login correctly
+            //return to login page
+            redirect(new \moodle_url('/login'), 'login fail');
         }
 
-
-        $this->params = $this->getTokenAndUserProfile();
-        if(!is_null($this->params->accessToken) and  $this->params->userProfile!==0) {
-
-            $this->handlelogin();
+            $this->handlelogin($userInfo);
 
             //its all right and user is redirected to dashboardo Moodle
             redirect(new \moodle_url('/my'));
 
-        }else{
-            redirect($dev_login_redirect);
         }
-    }
 
 
 }
