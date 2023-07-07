d27c31edef753dc3f1c1798ff092b5fad8cf048c
srssaulo <srssaulo@gmail.com>
2023-07-07 16:18:37 -0300

    teste redirect logout

--- classes/Connect.php
+++ classes/Connect.php
@@ -140,7 +140,6 @@ class Connect
     }
 
     public static function logout_govbr(){
-        echo 'Logout Moodle '; die;
         $redirect_uri = self::POST_LOGOUT_REDIRECT_URI;
         $logout_request = self::REDIRECT_URI;
         $action = $redirect_uri."?post_logout_redirect_uri=".$logout_request;
