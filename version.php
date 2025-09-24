89506252fdb4aec238dfd0c76da883d7c28dca8f
srssaulo <srssaulo@gmail.com>
2025-09-24 11:21:08 -0300

    increase plugin version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version    = 2023020605;
+$plugin->version    = 2023020606;
 $plugin->release   = '1.1';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires   = 2022041903;
